21df954ceec845ceedee3406a9f61caf41b077e6
Somvit09 <developer.09.sam@gmail.com>
2023-09-28 19:31:06 +0530

    edited

--- get_product_details.py
+++ get_product_details.py
@@ -4,10 +4,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import Chrome
+from selenium.common.exceptions import NoSuchElementException
+
+# Set up Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36")
+
 
 # initializing driver
-driver_path = Service('chromedriver')
-driver = webdriver.Chrome(service=driver_path)
+driver_path = Service('./chromedriver')
+driver = webdriver.Chrome(options=chrome_options)
 
 # url 
 url = 'https://www.amazon.in/s?rh=n%3A6612025031&fs=true&ref=lp_6612025031_sar'
@@ -41,34 +49,45 @@ def scrape():
     for i in all_product_links:
         # getting the link
         driver.get(i)
-        # scraping the information
-        product_title = driver.find_element(By.ID, 'title').text
-        product_rating = driver.find_element(By.XPATH, '//*[@id="reviewsMedley"]/div/div[1]/span[1]/span/div[3]/span').text[:-14]
-        product_price = driver.find_element(By.CLASS_NAME, '_p13n-desktop-sims-fbt_price_p13n-sc-price__bCZQt').text
-        product_seller = driver.find_element(By.ID, 'merchant-info').text[8:][:-25]
-        # adding those to the list
-        product_titles.append(product_title)
-        product_prices.append(product_price)
-        product_ratings.append(product_rating)
-        product_sellers.append(product_seller)
-        count += 1
+        try:
+            # scraping the information
+            product_title = driver.find_element(By.ID, 'title').text
+            product_rating = driver.find_element(By.XPATH, '//*[@id="reviewsMedley"]/div/div[1]/span[1]/span/div[3]/span').text[:-14]
+            product_price = driver.find_element(By.CLASS_NAME, '_p13n-desktop-sims-fbt_price_p13n-sc-price__bCZQt').text
+            product_seller = driver.find_element(By.ID, 'merchant-info').text[8:][:-25]
 
-    # creating a dataframe using pandas
-    dataframe = pd.DataFrame({'Product Name': product_titles, 
-                            'Price': product_prices,
-                            'Seller Name': product_sellers,
-                            'Rating': product_ratings
-                            })
+            # adding those to the list
+            product_titles.append(product_title)
+            product_prices.append(product_price)
+            product_ratings.append(product_rating)
+            product_sellers.append(product_seller)
+            count += 1
 
-    # making csv file
-    csv_file = dataframe.to_csv('product_details.csv')
+            print(product_titles, product_prices, product_ratings, product_sellers, count)
+
+            # creating a dataframe using pandas
+            dataframe = pd.DataFrame({'Product Name': product_titles, 
+                                    'Price': product_prices,
+                                    'Seller Name': product_sellers,
+                                    'Rating': product_ratings
+                                    })
+
+            # making csv file
+            dataframe.to_csv('product_details_new.csv')
+            print(count, len(all_product_links))
+        except NoSuchElementException:
+            product_title = None
+            product_rating = None
+            product_price = None
+            product_seller = None
+        except ConnectionRefusedError:
+            driver.quit()
 
     # closing the driver
     driver.quit()
     return count
 
-scrape_count = scrape()
-while scrape_count != len(all_product_links):
+while True:
     scrape_count = scrape()
     if scrape_count == len(all_product_links):
         break
